0404606132a372e400882b829a160dcc30945986
dmitriymaksimenko88@gmail.com <dmitriymaksimenko88@gmail.com>
2024-02-22 09:24:09 +0200

    [2-2] - API + Added ToDo Controller

---
+++ To-DoAPI/Controllers/ToDoController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using To_DoAPI.Models;
+
+namespace To_DoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDo>>> GetToDoSet()
+        {
+            return await _context.ToDoSet.ToListAsync();
+        }
+
+        // GET: api/ToDo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDo>> GetToDo(int id)
+        {
+            var toDo = await _context.ToDoSet.FindAsync(id);
+
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            return toDo;
+        }
+
+        // PUT: api/ToDo/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDo(int id, ToDo toDo)
+        {
+            if (id != toDo.ToDoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(await _context.ToDoSet.ToListAsync());
+        }
+
+        // POST: api/ToDo
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ToDo>> PostToDo(ToDo toDo)
+        {
+            _context.ToDoSet.Add(toDo);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.ToDoSet.ToListAsync());
+        }
+
+        // DELETE: api/ToDo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDo(int id)
+        {
+            var toDo = await _context.ToDoSet.FindAsync(id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoSet.Remove(toDo);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.ToDoSet.ToListAsync());
+        }
+
+        private bool ToDoExists(int id)
+        {
+            return _context.ToDoSet.Any(e => e.ToDoId == id);
+        }
+    }
+}
