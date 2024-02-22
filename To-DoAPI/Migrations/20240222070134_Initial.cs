74343b589932f342cdfaf252e02836727c07b14e
dmitriymaksimenko88@gmail.com <dmitriymaksimenko88@gmail.com>
2024-02-22 09:05:04 +0200

    [2-1] - API + Added ToDo Movel + Created Migration

---
+++ To-DoAPI/Migrations/20240222070134_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace To_DoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoSet",
+                columns: table => new
+                {
+                    ToDoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Desctiption = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeadLine = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoSet", x => x.ToDoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoSet");
+        }
+    }
+}
--- To-DoAPI/Models/ToDo.cs
+++ To-DoAPI/Models/ToDo.cs
@@ -1,6 +1,41 @@
-﻿namespace To_DoAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace To_DoAPI.Models
 {
     public class ToDo
     {
+        [Key]
+        public int ToDoId { get; set; } = 0;
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [StringLength(200)]
+        public string Desctiption { get; set; } = "";
+
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime DeadLine { get; set; }
+
+        private bool _status;
+        [Required]
+        public string Status
+        {
+            get 
+            {
+                if (_status == true) return "Done";
+                else return "Planned";
+            }
+            set 
+            {
+                if (value == "Done") _status = true;
+                else _status = false;
+            }
+        }
+
     }
 }
